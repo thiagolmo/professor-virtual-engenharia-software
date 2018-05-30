f9dc10e605d6d8010e6f865fd2daa7b6d1c05a48
Thiago Oliveira <tluizm@br.ibm.com>
2018-05-29 21:57:34 -0300

    Start to implement the login functionality

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const chatRouter = require('./routes/chat')
+const loginRouter = require('./routes/login')
 const bodyParser = require('body-parser')
+var session = require('client-sessions');
+var cfenv = require("cfenv");
 
 
 const serverPort = process.env.PORT || 3000
@@ -11,6 +14,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/chat',chatRouter)
+app.use('/login',loginRouter)
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -21,6 +26,53 @@ app.get('/login', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile('./views/chat.html',{root: __dirname});
 })
+
+app.use(session({
+  cookieName: 'sessionprofessorvirtual',
+  secret: 'random_string_goes_here',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+
+// load local VCAP configuration  and service credentials
+var vcapLocal;
+
+try {
+  vcapLocal = require('./vcap-local.json');
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
+if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
+  // Load the Cloudant library.
+  var Cloudant = require('cloudant');
+
+  // Initialize database with credentials
+  if (appEnv.services['cloudantNoSQLDB']) {
+     // CF service named 'cloudantNoSQLDB'
+     var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+  } else {
+     // user-provided service with 'cloudant' in its name
+     var cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);
+  }
+
+  //database name
+  var dbUser = 'professor-virtual-engenharia-software-users';
+
+  //Create the database.
+  cloudant.db.create(dbUser, function(err, data) {
+    if(!err) //err if database doesn't already exists
+      console.log("Created database: " + dbUser);
+  });
+
+  // Specify the database we are going to use (mydb)...
+  mydb = cloudant.db.use(dbUser);
+  myUsers = cloudant.db.use(dbUser);
+}
+
 
 
 app.listen(serverPort, function () {
---
+++ routes/login.js
@@ -0,0 +1,162 @@
+var express = require("express");
+var app = express();
+var cfenv = require("cfenv");
+var bodyParser = require('body-parser');
+var session = require('client-sessions');
+const router = express.Router();
+
+var vcapLocal;
+
+try {
+  vcapLocal = require('./vcap-local.json');
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
+if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
+  // Load the Cloudant library.
+  var Cloudant = require('cloudant');
+
+  // Initialize database with credentials
+  if (appEnv.services['cloudantNoSQLDB']) {
+     // CF service named 'cloudantNoSQLDB'
+     var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+  } else {
+     // user-provided service with 'cloudant' in its name
+     var cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);
+  }
+
+  myUsers = cloudant.db.use(dbUser);
+
+}
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+var email;
+var password;
+
+
+router.post('/checkUser', (request, response) => {
+  var email = request.body.email;
+  var password = request.body.password;
+  var status = false;
+
+  //Check user and email on database
+  myUsers.list({ include_docs: true }, function(err, body) {
+    if (!err) {
+      body.rows.forEach(function(row) {
+        if(row.doc.email == email)
+          if (row.doc.password == password){
+            status = true;
+          }
+      });
+  }
+    if (status == true){
+      console.log(status);
+      request.sessionprofessorvirtual = { 'email': email, 'password': password};
+      response.send({'login': 'ok'});
+    }else {
+      request.sessionprofessorvirtual = { 'email': email, 'password': password};
+      response.send({'login': 'nok'});
+    }
+
+  });
+
+
+});
+
+
+app.post("/api/login", function (request, response) {
+  var email = request.body.email;
+  var password = request.body.password;
+
+  if(!myUsers) {
+    console.log("No database.");
+    response.send("No database.");
+    return;
+  }
+  var status = false;
+  myUsers.list({ include_docs: true }, function(err, body) {
+    if (!err) {
+      body.rows.forEach(function(row) {
+        if(row.doc.userEmail == email)
+          if (row.doc.userPassword == password){
+            request.session.user = email;
+            request.session.password = password;
+            status = true;
+          }
+      });
+      if (status == true)
+        response.send(true);
+      else {
+        request.session.user = email;
+        request.session.password = password;
+        response.send(false);
+      }
+      }
+    });
+  });
+
+  app.get('/validSession', function(request, response) {
+  var status = false;
+  if ( request.session.user) { // Check if session exists
+    myUsers.list({ include_docs: true }, function(err, body) {
+      if (!err) {
+        body.rows.forEach(function(row) {
+          if(row.doc.userEmail == request.session.user)
+            if (row.doc.userPassword == request.session.password){
+              status = true;
+            }
+        });
+        if (status == true)
+          response.send(true);
+        else {
+          response.send(false);
+        }
+        }
+      });
+  } else {
+    response.send(false);
+  }
+});
+
+app.get('/destroySession', function(request,response) {
+  request.session.destroy(function(err) {
+    response.send(false);
+  })
+  response.send(true);
+});
+
+
+
+module.exports = router;
+
+// $("#idSubmitLogin").click(function(){
+//   email = document.getElementById('inputEmail').value;
+//   password = document.getElementById('inputPassword').value;
+//   //POST request to API to create a new visitor entry in the database
+//   $.ajax({
+// 				  method: "POST",
+// 				  url: "./routes/login",
+// 				  contentType: "application/json",
+// 				  data: JSON.stringify({email: email,
+//                                 password:password},
+//           )
+// 				})
+//                 .done(function(data) {
+//                   console.log(data);
+//                   if (data == true)
+//                     $(location).attr('href', '/chat.html')
+//                   else{
+//                     document.getElementById('idAlertUserPasswordInvalid').style.display = 'inline';
+//                     document.getElementById('idSpace').style.display = 'inline';}
+//                 });
+// });
